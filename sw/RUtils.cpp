d0e12a4002a3cfa88e9893f7770391af2fd3df94
dma1982 <mada.jlu@hotmail.com>
2011-07-10 13:54:10 +0800

    function split

--- sw/RUtils.cpp
+++ sw/RUtils.cpp
@@ -4,38 +4,6 @@
 
 using namespace std;
 
-string sw::gethostname()
-{
-    buffer_t buf;
-
-    int rc = ::gethostname(buf.data(), buf.capacity());
-    sw_assert(rc == 0, ERR_GET_HOSTNAME_FAILED);
-
-    buf.size(strlen(buf.data()) + 1);
-
-    return buf.data();
-}
-
-pid_t sw::spawn(const char* cmd, char*const* args)
-{
-    pid_t rc;
-#ifdef LIN
-    rc = vfork();
-    if (rc == 0)
-    {
-        sw_assert(execvp(cmd, args) > 0);
-    }
-    
-    sw_assert(rc > 0, ERR_CREATE_PROCESS_FAILED);
-
-#endif
-
-#ifdef WIN
-    
-#endif
-
-    return rc;
-}
 
 sw::handle_t sw::open(const string& fname, mode_t mode)
 {
--- sw/process.h
+++ sw/process.h
@@ -1,12 +1,15 @@
 #ifndef _SW_PROCESS_H_
 #define _SW_PROCESS_H_
 
+#include "process.h"
+
+
 namespace sw
 {
     std::string gethostname();
 
     // create a process
-    pid_t spawn(const char* cmd, char*const* args);
+    pid_t spawn(const char* cmd, char* const* args);
 
     // waitfor the process
     int wait(const pid_t& p);
