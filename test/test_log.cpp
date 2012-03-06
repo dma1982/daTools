9d72284eef58933951bb884988ae389c44cdd70c
Da Ma <mada.jlu@hotmail.com>
2012-03-06 20:02:03 +0800

    add test_log case

--- test/test_log.cpp
+++ test/test_log.cpp
@@ -6,7 +6,5 @@ int main(int argc, char** argv)
 {
     Logger* log = Logger::getLogger("mada.class");
     log->Debug("this is the first line");
-    //log->Debug("this is the %s line", "second");
     log->Assert(1!=1);
-    log->printStackTrace();
 }
