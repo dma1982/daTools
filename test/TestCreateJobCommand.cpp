03ec0c5bb8af3a8f84d9524fb65cf12776ccfeb3
dma1982 <mada.jlu@hotmail.com>
2012-10-27 17:05:44 +0800

    add UT framework for CreateJobCommand

--- test/TestCreateJobCommand.cpp
+++ test/TestCreateJobCommand.cpp
@@ -1,4 +1,15 @@
 #include <gtest/gtest.h>
 
+#include "ogl.h"
+#include "jm/Commands.h"
+
+TEST(CreateJobCommand, constructor)
+{
+    ogl::Command* cmd = new ogl::CreateJob();
+}
 
 
+int main(int argc, char **argv) {
+   ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
