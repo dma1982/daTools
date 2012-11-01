2b8ffa84263d67cc6ff129eaa0bde28ea5a21754
dma1982 <mada.jlu@hotmail.com>
2012-11-01 16:14:03 +0800

    JobOption serialize/deserialize UT

--- test/TestJobOption.cpp
+++ test/TestJobOption.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <cstring>
+using namespace std;
 
 TEST(JobOption, constructor)
 {
@@ -46,9 +47,40 @@ TEST(JobOption, serialize)
 
     ASSERT_TRUE(memcmp(msg->rd_ptr(), os.begin()->rd_ptr(), sizeof(ogl::Command)) == 0);
 
+    ogl::CreateJob* c = new ogl::CreateJob();
+    c->deserialize(msg);
+
+    ASSERT_EQ(strcmp(name, c->getJobOption()->name()), 0);
+    ASSERT_EQ(c->getJobOption()->priority(), 1);
+
+    msg->release();
+    delete cmd;
+    delete c;
+}
+
+TEST(JobOption, deserialize)
+{
+    ogl::CreateJob* cmd = new ogl::CreateJob();
+    ASSERT_TRUE(cmd !=0);
+    ogl::JobOption* jobOption = new ogl::JobOption();
+    cmd->setJobOption(jobOption);
+    jobOption->priority(1);
+    char* name = new char[128];
+    strncpy(name, "dma", 4);
+    jobOption->name(name);
+
+    ACE_Message_Block* msg = cmd->serialize();
+
+    ACE_OutputCDR os(ACE_DEFAULT_CDR_BUFSIZE);
+
+    os << ACE_CDR::ULong(ogl::CreateJobCommand);
+
+    ASSERT_TRUE(memcmp(msg->rd_ptr(), os.begin()->rd_ptr(), sizeof(ogl::Command)) == 0);
+
     msg->release();
     delete cmd;
 }
+
 
 int main(int argc, char **argv) {
    ::testing::InitGoogleTest(&argc, argv);
