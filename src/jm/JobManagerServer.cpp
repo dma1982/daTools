65d955c05361211f6a873605479c889a0eb85560
dma1982 <mada.jlu@hotmail.com>
2012-11-02 17:48:02 +0800

    job manager server framework.

---
+++ src/jm/JobManagerServer.cpp
@@ -0,0 +1,18 @@
+#include "JobManagerServer.h"
+
+namespace ogl
+{
+
+    JobManagerServer* JobManagerServer::m_instance = new JobManagerServer();
+
+    Executor* JobManagerServerHandler::executor()
+    {
+        return JobManagerServer::instance();
+    }
+
+    JobManagerServer* JobManagerServer::instance()
+    {
+        return m_instance;
+    }
+
+}
