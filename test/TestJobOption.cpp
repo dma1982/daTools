97966379583c007b2af87a3dda646a0eb15c93b8
dma1982 <mada.jlu@hotmail.com>
2012-11-06 00:03:48 +0800

    fix build error : CreateJob is not Serializable anymore

--- test/TestJobOption.cpp
+++ test/TestJobOption.cpp
@@ -27,61 +27,6 @@ TEST(JobOption, empty_serialize)
     msg->release();
 }
 
-
-TEST(JobOption, serialize)
-{
-    ogl::CreateJob* cmd = new ogl::CreateJob();
-    ASSERT_TRUE(cmd !=0);
-    ogl::JobOption* jobOption = new ogl::JobOption();
-    cmd->setJobOption(jobOption);
-    jobOption->priority(1);
-    char* name = new char[128];
-    strncpy(name, "dma", 4);
-    jobOption->name(name);
-
-    ACE_Message_Block* msg = cmd->serialize();
-
-    ACE_OutputCDR os(ACE_DEFAULT_CDR_BUFSIZE);
-
-    os << ACE_CDR::ULong(ogl::CreateJobCommand);
-
-    ASSERT_TRUE(memcmp(msg->rd_ptr(), os.begin()->rd_ptr(), sizeof(ogl::Command)) == 0);
-
-    ogl::CreateJob* c = new ogl::CreateJob();
-    c->deserialize(msg);
-
-    ASSERT_EQ(strcmp(name, c->getJobOption()->name()), 0);
-    ASSERT_EQ(c->getJobOption()->priority(), 1);
-
-    msg->release();
-    delete cmd;
-    delete c;
-}
-
-TEST(JobOption, deserialize)
-{
-    ogl::CreateJob* cmd = new ogl::CreateJob();
-    ASSERT_TRUE(cmd !=0);
-    ogl::JobOption* jobOption = new ogl::JobOption();
-    cmd->setJobOption(jobOption);
-    jobOption->priority(1);
-    char* name = new char[128];
-    strncpy(name, "dma", 4);
-    jobOption->name(name);
-
-    ACE_Message_Block* msg = cmd->serialize();
-
-    ACE_OutputCDR os(ACE_DEFAULT_CDR_BUFSIZE);
-
-    os << ACE_CDR::ULong(ogl::CreateJobCommand);
-
-    ASSERT_TRUE(memcmp(msg->rd_ptr(), os.begin()->rd_ptr(), sizeof(ogl::Command)) == 0);
-
-    msg->release();
-    delete cmd;
-}
-
-
 int main(int argc, char **argv) {
    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
